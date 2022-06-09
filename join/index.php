6b7463a78b807169662d17928a84929900ecb7e1
unknown <n1210043@jn2.iwasaki.ac.jp>
2022-06-10 01:01:51 +0900

    入力値保存無効バク修正
    誤　｝//54行目
    正    }  //49行目

--- join/index.php
+++ join/index.php
@@ -46,12 +46,13 @@ if(!empty($_POST)) {
         header('Location: check.php');
         exit();
     }
+}
 // 書き直し
 if ($_REQUEST['action'] == 'rewrite') {
     $_POST = $_SESSION['join'];
     $error['rewrite'] = true;
     }
-}
+
 ?>
 <p>次のフォームに必要事項をご記入ください。</p>
 <form action="" method="post" enctype="multipart/form-data">
