18138dfc231dfe7442a16997d2f208905c6da361
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2022-06-13 18:01:27 +0900

    返信機能追加

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 require('dbconnect.php');
 
-//error_reporting(0);
+error_reporting(0);
 if(isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
     //ログインしている
     $_SESSION['time'] = time();
@@ -20,19 +20,31 @@ if(isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
 //投稿を記録する
 if(!empty($_POST)){
     if($_POST['message'] != ''){
-        $sql = sprintf('INSERT INTO posts SET member_id=%d,message="%s",created=NOW()',
+        $sql = sprintf('INSERT INTO posts SET member_id=%d,message="%s",
+        reply_post_id=%d,created=NOW()',
         mysqli_real_escape_string($db,$member['id']),
-       mysqli_real_escape_string($db, $_POST['message'])
+       mysqli_real_escape_string($db, $_POST['message']),
+       mysqli_real_escape_string($db, $_POST['reply_post_id'])
     );
     mysqli_query($db,$sql) or die(mysqli_error($db));
     header('Location: index.php');
     exit();
     }
 }
-
+//投稿を取得する
 $sql = sprintf('SELECT m.name,m.picture,p.* FROM members m,
         posts p WHERE m.id = p.member_id ORDER BY p.created DESC');
 $posts = mysqli_query($db,$sql) or die(mysqli_error($db));        
+
+if (isset($_REQUEST['res'])){
+    $sql = sprintf('SELECT m.name,m.picture,p.* FROM members m,posts p WHERE 
+    m.id=p.member_id AND p.id = %d ORDER BY p.created DESC'
+    ,mysqli_real_escape_string($db,$_REQUEST['res'])
+    );
+    $record = mysqli_query($db,$sql) or die(sqli_error($db));
+    $table = mysqli_fetch_assoc($record);
+    $message = '@'.$table['name'].' '.$table['message'];
+}
 ?>
 <!DOCUTYPE html>
 <html>
@@ -50,7 +62,10 @@ $posts = mysqli_query($db,$sql) or die(mysqli_error($db));
                 <dl>
                 <dt><?php echo htmlspecialchars($member['name']); ?>さん、メッセージをどうぞ</dt>
                     <dd>
-                        <textarea name="message"  cols="50" rows="5"></textarea>
+                        <textarea name="message"  cols="50" rows="5"><?php 
+                            echo htmlspecialchars($message,ENT_QUOTES,'UTF-8'); ?></textarea>
+                            <input type = "hidden" name="reply_post_id" 
+                            value="<?php echo htmlspecialchars($_REQUEST['res'],ENT_QUOTES,'UTF-8');?>" />
                     </dd>
                 </dl>
                 <div>
@@ -65,7 +80,8 @@ $posts = mysqli_query($db,$sql) or die(mysqli_error($db));
                 <img src="member_picture/<?php echo htmlspecialchars($post['picture'],ENT_QUOTES,'UTF-8'); ?>" 
                 width="48" height="48" alt="<?php echo htmlspecialchars($post['picture'],ENT_QUOTES,'UTF-8'); ?>"/>
                 <p><?php echo htmlspecialchars($post['message'],ENT_QUOTES,'UTF-8'); ?>
-                <span class="name">(<?php echo htmlspecialchars($post['name'],ENT_QUOTES,'UTF-8'); ?>)</span></p>
+                <span class="name">(<?php echo htmlspecialchars($post['name'],ENT_QUOTES,'UTF-8'); ?>)</span>
+                [<a href="index.php?res=<?php echo htmlspecialchars($post['id'],ENT_QUOTES,'UFT-8'); ?>">Re</a>]</p>
                 <p class="day"><?php echo htmlspecialchars($post['created'],ENT_QUOTES,'UTF-8'); ?></p>
             </div>
             <?php endwhile; ?>
