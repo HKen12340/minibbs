9bcb455a5444ce25ef1f3b04068b93cc19efdd10
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2022-06-14 19:45:04 +0900

    ページング

--- index.php
+++ index.php
@@ -31,8 +31,27 @@ if(!empty($_POST)){
     }
 }
 //投稿を取得する
+
+$page = $_REQUEST['page'];
+if($page == ''){
+    $page = 1;
+}
+$page = max($page, 1);
+$page = max($page, 1);
+
+//最終ページを取得する
+$sql = 'SELECT COUNT(*) AS cnt FROM posts';
+$recordSet = mysqli_query($db, $sql);
+$table = mysqli_fetch_assoc($recordSet);
+$maxPage = ceil($table['cnt'] / 5);
+$page = min($page, $maxPage);
+
+$start = ($page - 1) * 5;
+$start = max(0, $start);
+
+
 $sql = sprintf('SELECT m.name,m.picture,p.* FROM members m,
-        posts p WHERE m.id = p.member_id ORDER BY p.created DESC');
+        posts p WHERE m.id = p.member_id ORDER BY p.created DESC LIMIT %d , 5',$start);
 $posts = mysqli_query($db,$sql) or die(mysqli_error($db));        
 
 //返信の場合
@@ -48,6 +67,12 @@ if (isset($_REQUEST['res'])){
 //htmlspecialcharsのショートカット
 function h($value){
     return htmlspecialchars($value,ENT_QUOTES,'UTF-8');
+}
+
+//本文内のURLリンクを設定します
+function makeLink($value){
+    return mb_ereg_replace("(https?)(://[[:alnum:]¥+¥$¥;¥?¥.%,!#~*/:@&=_-]+)", 
+    '<a href="\1\2">\1\2</a>' , $value);
 }
 ?>
 <!DOCUTYPE html>
@@ -78,12 +103,11 @@ function h($value){
             </form>
             <?php 
                 while($post = mysqli_fetch_assoc($posts)):
-                   
             ?>
             <div class="msg">
                 <img src="member_picture/<?php  echo  h($post['picture']); ?>" 
                 width="48" height="48" alt="<?php h($post['picture']);?>"/>
-                <p><?php echo  h($post['message']); ?>
+                <p><?php echo makeLink(h($post['message'])); ?>
                 <span class="name">(<?php echo  h($post['name']); ?>)</span>
                 [<a href="index.php?res=<?php echo  h($post['id']); ?>">Re</a>]</p>
                 <p class="day"><a href="view.php?id=<?php  echo  h($post['id']); ?>">
@@ -91,8 +115,34 @@ function h($value){
                 <?php if($post['reply_post_id']>0): ?>
                     <a href="view.php?id=<?php echo  h($post['reply_post_id']); ?>">送信元のメッセージ</a>
                 <?php endif; ?>
+                <?php if($_SESSION['id'] == $post['member_id']): ?>
+                    [<a href="delete.php?id=<?php echo h($post['id']); ?>" 
+                    style="color:#F33;">削除</a>]
+                <?php endif; ?>
             </div>
             <?php endwhile; ?>
+            <ul class="paging">
+            <?php 
+                if($page > 1){
+            ?><li><a href="index.php?page=<?php print($page - 1); ?>">前のページへ</a></li>
+            <?php 
+                }else{ 
+            ?>
+                <li>前のページへ</li>
+            <?php
+                 } 
+             ?>
+             <?php 
+                if($page < $maxPage){
+            ?><li><a href="index.php?page=<?php print($page + 1); ?>">次のページへ</a></li>
+            <?php 
+                }else{ 
+            ?>
+                <li>次のページへ</li>
+            <?php
+                 } 
+             ?>
+            </ul>
         </div>
         <div id="foot">
             <p><img src="images/txt_copyright.png" width="136" height="15" alt="(C) H2O SPACE, Mynavi"/></p>
